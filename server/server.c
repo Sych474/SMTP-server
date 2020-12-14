2b788af26956f05edfe9bd989f0f14e3a4a967cb
Sychev Svyatoslav <rk474@yandex.ru>
2020-12-14 12:30:59 -0800

    #1 add exit-keyword to server

--- server/server.c
+++ server/server.c
@@ -12,6 +12,11 @@
 #define on_error(...) { fprintf(stderr, __VA_ARGS__); fflush(stderr); exit(1); }
 
 
+void debug_print(char* message, int message_len)
+{
+    printf("MSG: %s [%d]\n",message, message_len);
+}
+
 int main (int argc, char *argv[]) {
     if (argc < 2) 
         on_error("Usage: %s [port]\n", argv[0]);
@@ -21,6 +26,10 @@ int main (int argc, char *argv[]) {
     int server_fd, client_fd;
     struct sockaddr_in server, client;
     char buf[BUFFER_SIZE];
+    char message[BUFFER_SIZE];
+    int message_size = 0; 
+    char *keystring = "exit";
+    int keystring_len = strlen(keystring);
 
     server_fd = socket(AF_INET, SOCK_STREAM, 0);
     if (server_fd < 0) 
@@ -50,15 +59,52 @@ int main (int argc, char *argv[]) {
             on_error("Could not establish new connection\n");
 
         while(1) {
+            memset(buf, 0, BUFFER_SIZE);
             int read = recv(client_fd, buf, BUFFER_SIZE, 0);
 
             if (!read)
                 break;
+
             if (read < 0) 
                 on_error("Client read failed\n");
 
-            if (send(client_fd, buf, read, 0) < 0) 
-                on_error("Client write failed\n");
+            int real_read = read;
+
+            char* key_position = strstr(buf,keystring);
+            if (key_position == NULL)
+            {
+                // no end keyword
+                memcpy(message + message_size, buf, real_read);
+                message_size += real_read;
+                debug_print(message, message_size);       
+            }
+            else 
+            {
+                // have end keyword
+                int end_len = key_position - buf;
+                memcpy(message + message_size, buf, end_len);
+                message_size += end_len;
+                message[message_size] = '\n';
+                message_size++; 
+                printf("END FOUND, send Data:\n");
+                debug_print(message, message_size);       
+                
+                if (send(client_fd, message, message_size, 0) < 0) 
+                    on_error("Client write failed\n");
+
+                memset(message,0, BUFFER_SIZE);
+                message_size = 0;
+                
+                
+                char* next_message_start_ptr = key_position + keystring_len;
+                int next_message_len = strlen(next_message_start_ptr);
+                memcpy(message, next_message_start_ptr, next_message_len);
+                message_size = next_message_len; 
+                message[message_size] = '\n';
+                message_size++; 
+                printf("START NEW MESSAGE:\n");
+                debug_print(message, message_size); 
+            }
         }
     }
 
