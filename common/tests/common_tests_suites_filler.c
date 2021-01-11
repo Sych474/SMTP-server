ce7c55455a6ccc902ca68c7f6052c99e3d8a4117
Sychev Svyatoslav <rk474@yandex.ru>
2021-01-11 12:53:00 -0800

    add memory checks and fix leaks in tests

--- common/tests/common_tests_suites_filler.c
+++ common/tests/common_tests_suites_filler.c
@@ -1,7 +1,9 @@
 #include "common_tests_suites_filler.h"
 
 int fill_common_test_suites() {
+
     int result;
+
     CU_pSuite address_suite = CU_add_suite("Address unit tests", address_test_init, address_test_free);
     if (!address_suite) {
         CU_cleanup_registry();
