0d649c10edcd7664b9b560e2431ab5dab70c6dcb
Sychev Svyatoslav <rk474@yandex.ru>
2021-01-11 08:57:17 -0800

    test xml outpoot -> txt output

--- server/src/tests/unit/server_tests_main.c
+++ server/src/tests/unit/server_tests_main.c
@@ -1,8 +1,6 @@
 #include "server_tests_suites_filler.h"
 #include "common_tests_suites_filler.h"
 
-#define C_UNIT_FILENAME_ROOT "CUnit"
-
 int main(int argc, char *argv[]) {
     printf("Starting Server tests..\n");
 
@@ -23,9 +21,6 @@ int main(int argc, char *argv[]) {
         return res;
     }
 
-    CU_set_output_filename(C_UNIT_FILENAME_ROOT);
-    CU_list_tests_to_file();
-    CU_automated_run_tests();
 
     CU_basic_set_mode(CU_BRM_VERBOSE);
     CU_basic_run_tests();
